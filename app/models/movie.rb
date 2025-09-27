1b59b12f2bf64ffad4f1fe5754315f7bde288f4c
Benjamin Benscher <beb2181@columbia.edu>
2025-09-27 17:46:39 +0000

    fix

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,7 +5,7 @@ class Movie < ActiveRecord::Base
 
   def self.with_ratings(ratings_list)
     return all if ratings_list.blank?
-    where('LOWER(rating) IN (?)', Array(ratings_list).map(&:downcase))  end
+    where('LOWER(rating) IN (?)', Array(ratings_list).map(&:downcase))
   #used gpt5 for syntax help and debugging
   end
 end
